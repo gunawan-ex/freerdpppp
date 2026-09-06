618f81c916f785ba1ed69d706a334a5eceb3bd27
Gunawan <110682243+gunawan-ex@users.noreply.github.com>
2026-09-06 19:59:40 +0800

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -25,7 +25,7 @@ del /q "C:\Users\Public\Desktop\Epic Games Launcher.lnk" >nul 2>&1
 del /q "C:\Users\Public\Desktop\Unity Hub.lnk" >nul 2>&1
 
 set "password=nk0ckhun9"
-powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
+powershell -Command "Import-Module Microsoft.PowerShell.Security; Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"
 
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
 
