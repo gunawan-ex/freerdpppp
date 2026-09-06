77e9aa1913fe1567e19578967c9edeb5b6feab6e
Gunawan <110682243+gunawan-ex@users.noreply.github.com>
2026-09-06 20:11:53 +0800

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,4 +1,5 @@
 @echo off
-echo Alive 👾...
-:recheck
-tasklist | find /i "provisioner.exe" >Nul && (ping 127.0.0.1 >Nul & goto recheck)  || echo "Dead 💀..."
+echo Runner aktif. Menjaga sesi tetap menyala...
+:loop
+timeout /t 60 /nobreak >nul
+goto loop
