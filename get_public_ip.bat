1d3aade5e34595e038283a33bed3c0fe8f53cdbc
Gunawan <110682243+gunawan-ex@users.noreply.github.com>
2026-09-06 20:23:08 +0800

    Update get_public_ip.bat

--- get_public_ip.bat
+++ get_public_ip.bat
@@ -1,14 +1,11 @@
 @echo off
 setlocal
 
-REM File to save JSON
 set "JSONFILE=%TEMP%\public_ip.json"
 if exist "%JSONFILE%" del "%JSONFILE%" >nul 2>&1
 
-REM --- Run PowerShell as a single line ---
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command " $ip = Invoke-RestMethod 'https://api.ipify.org'; @{ip=$ip} | ConvertTo-Json | Out-File '%JSONFILE%' -Encoding UTF8 "
 
-REM Display the JSON
 if exist "%JSONFILE%" (
     echo Public IP JSON:
     type "%JSONFILE%"
@@ -16,5 +13,4 @@ if exist "%JSONFILE%" (
     echo Failed to retrieve public IP.
 )
 
-pause
 endlocal
